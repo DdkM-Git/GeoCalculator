b2f58dd5ca3fae9811cf24287a70fe57b7a712b4
DdkM-Git <55554224+DdkM-Git@users.noreply.github.com>
2026-02-02 17:22:19 +0100

    Poprawienie walidatora współrzędnych i komunikatów błędów

--- backend/src/Validator/CoordinateValidator.php
+++ backend/src/Validator/CoordinateValidator.php
@@ -4,20 +4,30 @@ namespace MateuszDudek\Backend\Validator;
 
 class CoordinateValidator
 {
+    private const LAT_MIN = -90;
+    private const LAT_MAX = 90;
+    private const LNG_MIN = -180;
+    private const LNG_MAX = 180;
+
     public static function validate(array $data): void
     {
         foreach (['pointA', 'pointB'] as $point) {
-            if (!isset($data[$point]['lat'], $data[$point]['lng'])) {
-                throw new \InvalidArgumentException('Brak współrzędnych');
-            }
+            self::validatePoint($point, $data[$point] ?? null);
+        }
+    }
 
-            if ($data[$point]['lat'] < -90 || $data[$point]['lat'] > 90) {
-                throw new \InvalidArgumentException('Nieprawidłowa szerokość geograficzna');
-            }
+    private static function validatePoint(string $name, ?array $coords): void
+    {
+        if (!$coords || !isset($coords['lat'], $coords['lng'])) {
+            throw new \InvalidArgumentException("Brak współrzędnych dla punktu $name");
+        }
 
-            if ($data[$point]['lng'] < -180 || $data[$point]['lng'] > 180) {
-                throw new \InvalidArgumentException('Nieprawidłowa długość geograficzna');
-            }
+        if ($coords['lat'] < self::LAT_MIN || $coords['lat'] > self::LAT_MAX) {
+            throw new \InvalidArgumentException("Nieprawidłowa szerokość geograficzna dla punktu $name");
+        }
+
+        if ($coords['lng'] < self::LNG_MIN || $coords['lng'] > self::LNG_MAX) {
+            throw new \InvalidArgumentException("Nieprawidłowa długość geograficzna dla punktu $name");
         }
     }
 }
