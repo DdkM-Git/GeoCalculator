ac196db2b9e3e9d8d664ec3064a28168cfb3f1ab
DdkM-Git <55554224+DdkM-Git@users.noreply.github.com>
2026-02-02 13:23:07 +0100

    Dodanie minimalnej implementacji kalkulatora odległości, aby test przeszedł

---
+++ backend/src/Service/DistanceCalculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MateuszDudek\Backend\Service;
+
+class DistanceCalculator
+{
+    public function calculate(float $lat1, float $lon1, float $lat2, float $lon2): float
+    {
+        return 255000;
+    }
+}
