c1315d1828babc4a8b299a9fbe8b9f4671a7f43d
DdkM-Git <55554224+DdkM-Git@users.noreply.github.com>
2026-02-02 13:22:53 +0100

    Update DistanceCalculatorTest.php

--- backend/tests/DistanceCalculatorTest.php
+++ backend/tests/DistanceCalculatorTest.php
@@ -1,11 +1,18 @@
-public function testCalculatesDistanceBetweenTwoPoints()
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use MateuszDudek\Backend\Service\DistanceCalculator;
+
+class DistanceCalculatorTest extends TestCase
 {
-    $calculator = new DistanceCalculator();
+    public function testDistanceBetweenWarsawAndCracow()
+    {
+        $calc = new DistanceCalculator();
+        $distance = $calc->calculate(52.2297, 21.0122, 50.0647, 19.9450);
 
-    $distance = $calculator->calculate(
-        52.2297, 21.0122,
-        50.0647, 19.9450
-    );
-
-    $this->assertGreaterThan(250000, $distance);
+        $this->assertGreaterThan(250000, $distance);
+        $this->assertLessThan(260000, $distance);
+    }
 }
