f2dee9a62a192250bfed1bd04cca7bf601bc9908
DdkM-Git <55554224+DdkM-Git@users.noreply.github.com>
2026-02-02 17:23:57 +0100

    Dodanie nieprzechodzącego testu kontrolera obliczania odległości

---
+++ backend/tests/DistanceControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use MateuszDudek\Backend\Controller\DistanceController;
+
+class DistanceControllerTest extends TestCase
+{
+    public function testControllerReturnsDistance()
+    {
+        $controller = new DistanceController();
+
+        $response = $controller->calculate([
+            'pointA' => ['lat' => 52.2297, 'lng' => 21.0122],
+            'pointB' => ['lat' => 50.0647, 'lng' => 19.9450],
+        ]);
+
+        $this->assertArrayHasKey('distance', $response);
+        $this->assertArrayHasKey('meters', $response['distance']);
+        $this->assertArrayHasKey('kilometers', $response['distance']);
+    }
+}
