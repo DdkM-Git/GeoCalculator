f7950eb7667eab7febf1667dfecb99cf41ff6a47
DdkM-Git <55554224+DdkM-Git@users.noreply.github.com>
2026-02-05 10:28:53 +0100

    test nieprzechodzący CoordinateInput

---
+++ frontend/tests/CoordinateInput.spec.ts
@@ -0,0 +1,9 @@
+import { mount } from "@vue/test-utils";
+import CoordinateInput from "@/components/CoordinateInput.vue";
+
+it("emits update events", async () => {
+  const wrapper = mount(CoordinateInput);
+  await wrapper.find("input").setValue(50);
+
+  expect(wrapper.emitted()).toHaveProperty("update:lat");
+});
