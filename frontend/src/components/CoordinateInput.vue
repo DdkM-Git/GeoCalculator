d19a26d2f839d3438ea8f94531bd0eb05d176390
DdkM-Git <55554224+DdkM-Git@users.noreply.github.com>
2026-02-05 10:34:51 +0100

    Implementacja CoordinateInput

---
+++ frontend/src/components/CoordinateInput.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  lat: number | null;
+  lng: number | null;
+}>();
+
+defineEmits(["update:lat", "update:lng"]);
+</script>
+
+<template>
+  <input type="number" @input="$emit('update:lat', +($event.target as HTMLInputElement).value)" />
+  <input type="number" @input="$emit('update:lng', +($event.target as HTMLInputElement).value)" />
+</template>
